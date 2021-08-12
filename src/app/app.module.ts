5578b153483909bc4620689feff043b0a6034259
tss-legal-code <tss.legal.code@gmail.com>
2021-08-12 16:39:42 +0300

    finished tutorial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {CoursesComponent} from './courses.component';
+import { CourseComponent } from './course/course.component';
+import {CoursesService} from "./courses.service";
+import {EmailService} from "./email.service"
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoursesComponent,
+    CourseComponent,
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    CoursesService,
+    EmailService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/courses.service.ts
@@ -0,0 +1,5 @@
+export class CoursesService {
+  getCourses(){
+    return ['course_1', 'course_2', 'course_3', ]
+  }
+}
